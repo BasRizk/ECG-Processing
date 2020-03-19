4648aff0ae26e4e5bc0696f98fb031c0839cd910
Basem Rizk <basem.rizk@outlook.com>
2020-03-19 16:12:40 +0200

    init steps

--- QRS_Detector.py
+++ QRS_Detector.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
 """
 Apply your function to the ECG signal provided in the file “DataN.txt”.
 The sampling rate of this ECG signal is 256 Hz. You will need to suggest a
 method to compute the threshold needed for detection.
+
+
+TIPS:
+    - The sampling rate of this ECG signal is 256 Hz.
+    - N should be set approximately the same as the widest possible QRS
+complex
+    - 
 """
 
-def qrs_detect(raw_signal, win_size):
+def qrs_detect(raw_signal, win_size=0):
     """
-    
 
     Parameters
     ----------
@@ -22,4 +30,41 @@ def qrs_detect(raw_signal, win_size):
     contains the corresponding RR intervals
 
     """
+    diff = differentiate(raw_signal)
+    sqrd = square(diff)
+    smoothed = smooth(sqrd)
+    thresholded = threshold(smoothed)
+    rr_intervals = rr_define(thresholded)
+    return rr_intervals
+
+def differentiate(signal):
+    # TODO
     pass
+
+def square(signal):
+    # TODO
+    pass
+
+def smooth(signal):
+    # TODO
+    # smooth the squared signal using a moving average window
+    
+    pass
+
+def threshold(signal):
+    # TODO
+    pass
+
+def rr_define(rr_intervals):
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    raw_signal = pd.read_csv("DataN.txt", header=None)
+    rr_graph = qrs_detect(raw_signal) 
+    
+    
+    
+
+
