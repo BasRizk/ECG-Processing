2833ba4db72bfc1b205d80e35faa75c080fa76ad
Basem Rizk <basem.rizk@outlook.com>
2020-03-22 21:03:46 +0200

    Exporting figures, including r waves astrix marked ones

--- QRS_Detector.py
+++ QRS_Detector.py
@@ -11,18 +11,18 @@ method to compute the threshold needed for detection.
 
 TIPS:
     - The sampling rate of this ECG signal is 256 Hz.
-    - N should be set approximately the same as the widest possible QRS
+    - N (win_size) should be set approximately the same as the widest possible QRS
 complex
-    - 
 """
 
-def qrs_detect(raw_signal, win_size=15):
+def qrs_detect(raw_signal, win_size=15, save=False,
+               enable_noise_filter=True, plotlimit=2000):
     """
 
     Parameters
     ----------
     raw_signal : TYPE
-        DESCRIPTION.
+
     win_size : TYPE
         the moving average window size N.
 
@@ -33,45 +33,55 @@ def qrs_detect(raw_signal, win_size=15):
 
     """
     raw_signal = np.array(raw_signal)
-    noise_filtered_signal = remove_noise(raw_signal)
+    if enable_noise_filter:
+        noise_filtered_signal = remove_noise(raw_signal)
+        
+        plot_before_after(raw_signal, noise_filtered_signal,
+                          title = "Before_After_Filter",
+                          save=save, limit=plotlimit)
     
-    if(len(raw_signal) == 2000) :
-        end_time = raw_signal.shape[0]-1/256
-        t = np.linspace(0, end_time , raw_signal.shape[0])
-        plt.subplot(211)
-        plt.plot(t, raw_signal)
-        plt.subplot(212)
-        plt.plot(t, noise_filtered_signal)
-        plt.savefig("Before_After_Filter.jpg")
-        plt.show()
-    
-    diff = differentiate(noise_filtered_signal)
-    plot(diff, title="Differentiated Signal")
-    
+        diff = differentiate(noise_filtered_signal)
+    else:
+        diff = differentiate(raw_signal)
+        
+    plot(diff, title="Differentiated Signal", limit=plotlimit)
+
     sqrd = square(diff)
-    plot(sqrd, title="Squared Signal")
+    plot(sqrd, title="Squared Signal", limit=plotlimit)
 
     smoothed = smooth(sqrd, win_size)
-    plot(smoothed, title="Smoothed Signal")
+    plot(smoothed, title="Smoothed Signal", limit=plotlimit)
 
     thresholded, threshold1 = threshold(smoothed)
-    plot(thresholded, title="Thresholded Signal")
+    plot(thresholded, title="Thresholded Signal", limit=plotlimit)
 
     rr_intervals = rr_define(thresholded)
-    #plot(rr_intervals, title="RR-Intervals", sampling_rate=1)
+    plot(rr_intervals*1000, title="RR", sampling_rate=1,
+         xlabel='Beat Number', ylabel='time(ms)',
+         limit=rr_intervals.shape[0])
     
-    if(len(raw_signal) > 10000) :
-        end_time = rr_intervals.shape[0]-1
-        t = np.linspace(0, end_time , rr_intervals.shape[0])
-        plt.plot(t, rr_intervals*1000)
-        plt.xlabel('Beat Number')
-        plt.ylabel('time(ms)')
-        plt.title("RR Intervals")
-        plt.xlim([0, end_time])
-        plt.savefig("RR.jpg")
-        plt.show()
-        
+    r_markers = create_markers(rr_define(thresholded[:plotlimit]))
+    final_figure_title = "DetectedR_" + str(win_size)
+    if not enable_noise_filter:
+        final_figure_title = "Unfiltered_" + str(win_size)
+    plot(thresholded, title = final_figure_title , sampling_rate=256,
+         xlabel="t", ylabel="x(t)", markers=r_markers, save=save,
+         limit=plotlimit)
+
     return rr_intervals
+
+def create_markers(rr_intervals, sampling_rate=256):
+
+    r_markers = rr_intervals*256
+    first_might_miss_beat = np.argmax(r_markers)
+    if first_might_miss_beat not in r_markers:
+        tmp = np.zeros((r_markers.shape[0]+1,))
+        tmp[0] = first_might_miss_beat
+        tmp[1:] = r_markers
+        r_markers = tmp
+    for i in range(1, len(r_markers)):
+        r_markers[i] = r_markers[i] + r_markers[i-1]
+    return r_markers
 
 def remove_noise(sig):
     filtered_signal = notch_filter(sig, 50.0)
@@ -110,17 +120,8 @@ def differentiate(sig):
         (-t_0 - (2*t_1) + (2*t_3) + (t_4))
     return diff_signal
 
-# def differentiate2(sig):
-#     diff_signal = np.zeros(sig.shape)
-#     for i in range(2, len(sig)-2):
-#         diff_signal[i] = (1/8) * (-sig[i-2] - 2*sig[i-1] + 2*sig[i+1] + sig[i+2])
-#     return diff_signal
-
-
 def square(sig):
     return np.square(sig)
-
-
 
 def smooth(sig, win_size=5):
     # smooth the squared signal using a moving average window
@@ -134,14 +135,14 @@ def smooth(sig, win_size=5):
         # print("sig %s ...... smoothed_signal %s" % (sig[i], smoothed_signal[i]))
     return smoothed_signal
 
-def threshold(sig):
-    threshold = np.average(sig)*1.5
+def threshold(sig, avg_mult=1.5):
+    threshold = np.average(sig)*avg_mult
     # threshold = np.max(sig)*0.7
     thresholded = sig.copy()
     thresholded[thresholded > threshold] = threshold
     return thresholded, threshold
 
-def rr_define(sig, sampling_rate=256):
+def rr_define(sig, sampling_rate=256, discard_less_than=100):
     # peak_indices = np.argwhere(sig == np.amax(sig))
     
     rr_intervals = []
@@ -150,7 +151,7 @@ def rr_define(sig, sampling_rate=256):
 
     for v in sig:
         if v == peak_value:
-            if current_interval <= 100:
+            if current_interval <= discard_less_than:
                 # print("SAME" + str(len(rr_intervals)))
                 current_interval += 1
                 continue
@@ -161,22 +162,56 @@ def rr_define(sig, sampling_rate=256):
     rr_intervals = np.array(rr_intervals)/sampling_rate
     return rr_intervals
 
-def plot(sig, title = "Plot of CT signal", sampling_rate = 256):
-    end_time = sig.shape[0]-1/sampling_rate
-    t = np.linspace(0, end_time , sig.shape[0])
-    plt.plot(t, sig)
-    plt.xlabel('t')
-    plt.ylabel('x(t)')
+def plot(sig, title = "Plot of CT signal", sampling_rate=256,
+         xlabel="t", ylabel="x(t)", markers=None, save=False,
+         limit=2000):
+    draw_sig=sig[:limit]
+    end_time = draw_sig.shape[0]-1/sampling_rate
+    t = np.linspace(0, end_time , draw_sig.shape[0])
+    if markers is not None:
+        markers_amp = np.ones(markers.shape)*np.max(draw_sig)
+        plt.plot(markers, markers_amp, marker="*",
+                 linestyle=' ', color='r', label='R-Waves')
+    plt.plot(t, draw_sig)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.title(title)
-    plt.xlim([0, end_time])
-    #plt.savefig(title+".jpg")
+    plt.xlim([0, end_time])  
+    if save:
+        plt.savefig(title + ".jpg", progressive=True)
     plt.show()
-    
-
+        
+        
+def plot_before_after(before_sig, after_sig,
+                      title = "Before & After of CT signal",
+                      sampling_rate=256,
+                      save=False, limit=2000):
+    draw_before_sig = before_sig[:limit]
+    draw_after_sig = after_sig[:limit]
+    end_time = draw_before_sig.shape[0]-1/sampling_rate
+    t = np.linspace(0, end_time , draw_before_sig.shape[0])
+    plt.subplot(211)
+    plt.plot(t, draw_before_sig)
+    plt.subplot(212)
+    plt.plot(t, draw_after_sig)
+    if save:
+        plt.savefig(title + ".jpg", progressive=True)
+    plt.show()
 
 if __name__ == '__main__':
     raw_signal = pd.read_csv("DataN.txt", header=None)[0][:]
-    rr_graph = qrs_detect(raw_signal, win_size = 25)
+    rr_graph = qrs_detect(raw_signal, win_size = 25,
+                          save=True, enable_noise_filter=False,
+                          plotlimit=2000)
+    rr_graph = qrs_detect(raw_signal, win_size = 5,
+                          save=True, enable_noise_filter=True,
+                          plotlimit=2000)
+    rr_graph = qrs_detect(raw_signal, win_size = 15,
+                          save=True, enable_noise_filter=True,
+                          plotlimit=2000)
+    rr_graph = qrs_detect(raw_signal, win_size = 25,
+                          save=True, enable_noise_filter=True,
+                          plotlimit=2000)
         
     
     
